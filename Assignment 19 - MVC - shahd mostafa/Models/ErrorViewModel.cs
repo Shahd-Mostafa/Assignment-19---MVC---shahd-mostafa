4f7927e916ffc984317906b9a2ede19e8a592047
Shahd  Mostafa <shahd.mustafa124@gmail.com>
2025-03-11 23:37:04 +0200

    Add project files.

---
+++ Assignment 19 - MVC - shahd mostafa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignment_19___MVC___shahd_mostafa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
