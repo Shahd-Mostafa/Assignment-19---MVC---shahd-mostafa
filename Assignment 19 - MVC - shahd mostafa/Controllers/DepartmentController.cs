e77de4d3e457d551fe572fdb9fff4eb20c2529b5
Shahd  Mostafa <shahd.mustafa124@gmail.com>
2025-03-12 01:27:24 +0200

    V7

--- Assignment 19 - MVC - shahd mostafa/Controllers/DepartmentController.cs
+++ Assignment 19 - MVC - shahd mostafa/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Demo.BLL.Interfaces;
 using Demo.BLL.Repositories;
+using Demo.DAL.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Assignment_19___MVC___shahd_mostafa.Controllers
@@ -23,6 +24,16 @@ namespace Assignment_19___MVC___shahd_mostafa.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department) {
+            if (!ModelState.IsValid)
+            {
+                return View(department);
+            }
+            _repository.Add(department);
+            return RedirectToAction(nameof(Index));
         }
 
     }
