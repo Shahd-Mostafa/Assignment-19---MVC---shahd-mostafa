df52cc77c67f80ab01264b8281d66549c8e4e4c7
Shahd  Mostafa <shahd.mustafa124@gmail.com>
2025-03-12 00:15:10 +0200

    V1

---
+++ Demo.DAL/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Demo.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Context
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-FIK5SPH\\SQLEXPRESS;database=MVCDatabase;User Id=Shahd_Mostafa;Password=ColdYagami1258;TrustServerCertificate = True");
+        }
+
+        public DbSet<Department> departments { get; set; }
+    }
+}
