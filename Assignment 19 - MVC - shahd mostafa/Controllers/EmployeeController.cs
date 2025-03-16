f3d6586c15feb91539548612901271f459a30f36
Shahd  Mostafa <shahd.mustafa124@gmail.com>
2025-03-17 00:47:46 +0200

    V4

---
+++ Assignment 19 - MVC - shahd mostafa/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Demo.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_19___MVC___shahd_mostafa.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _repository;
+
+        public EmployeeController(IEmployeeRepository repository)
+        {
+            _repository = repository;
+        }
+        public IActionResult Index()
+        {
+            var Employees = _repository.GetAll();
+            return View(Employees);
+        }
+    }
+}
